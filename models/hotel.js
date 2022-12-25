49565b0f6a7c33ed3f683279761cf08fcc52073c
aloyuche <aloynwa@gmail.com>
2022-12-25 20:18:43 +0100

    hotel

---
+++ models/hotel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const hotelSchema = new mongoose.Schema(
+    {
+        name:{type: String, required: true},
+        type:{type: String, required: true},
+        city:{type: String, required: true},
+        distance:{type: String, required: true},
+        photos:{type: [String]},
+        description:{type: String, required: true},
+        title:{type: String, required: true},
+        rating:{type: Number, min: 0, max: 5,},
+        price:{type: Number, default: 0},
+        featured:{type: Boolean, default: false},
+        
+       
+    }
+)
+
+module.exports = mongoose.model("User", hotelSchema)
